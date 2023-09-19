880b7875077b0ff89c086f6f9cdea0a1e24f3c5d
sibeldiana <114567939+sibeldiana@users.noreply.github.com>
2023-09-19 23:57:44 +0100

    operators

---
+++ python-programs/calculator.py
@@ -0,0 +1,14 @@
+def calculate(n1,n2,op):
+    if op == '+':
+        result = n1+n2
+    elif op == '-':
+        result = n1-n2
+    elif op == '*':
+        result =  n1*n2
+    elif op == '/':
+        result = n1/n2
+    elif op=='^':
+        result =  n1**n2
+
+return result
+
